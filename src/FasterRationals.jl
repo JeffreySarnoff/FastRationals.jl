8feee2177f920768160390080be7a0603a043fae
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 10:12:17 +0000

    Update FasterRationals.jl
    #include(joinpath("types/mutablfast_rational.jl")

--- src/FasterRationals.jl
+++ src/FasterRationals.jl
@@ -9,9 +9,9 @@ import Base: convert, promote_rule, string, show,
     (+), (-), (*), (//), div, rem, fld, mod, cld,
     (==), (<), (<=), isequal, isless
 
-#include("types/struct/fast_rational.jl")
-#include("types/mutable/fast_rational.jl")
-#include("types/namedtuple/fast_rational.jl")
+#include(joinpath("types/struct","fast_rational.jl")
+#include(joinpath("types/mutablfeast_rational.jl")
+#include(joinpath("types/namedtuple/fast_rational.jl")
 
 #include(joinpath("types/namedtuple", "compares.jl"))
 #include(joinpath("types/struct", "compares.jl"))
