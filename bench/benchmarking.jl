722d844bd0f22c67e236a1c06b0becc12c3dc0d4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 14:32:36 +0000

    Update benchmarking.jl

--- bench/benchmarking.jl
+++ bench/benchmarking.jl
@@ -6,10 +6,10 @@ using BenchmarkTools
 
 #Example
 
-julia> hilbert_matrix(Rational, Int16, 2)
-2×2 Array{Rational{Int16},2}:
- 1//1  1//2
- 1//2  1//3
+julia> n = 16; 
+julia> @assert n <= 32 # otherwise overflows Int2128
+julia> amat = hilbert_matrix(Rational, Int128, n);
+julia> amat_factors = lufact(amat).factors;
 
 """
 function hilbert_matrix(::Type{Q}, ::Type{S}, isqrt_nxn::I) where {I, Q, S}
