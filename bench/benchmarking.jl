1cb046edc87e79cf84580fcc2e4e9725391eb7dc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 12:31:23 +0000

    Update benchmarking.jl

--- bench/benchmarking.jl
+++ bench/benchmarking.jl
@@ -1,1 +1,19 @@
+using FasterRationals
+using BenchmarkTools
+
+""
+    hilbert_square_matrix(::AkoRational, ::AkoInt, n_of_nxn)
+
+#Example
+
+julia> hilbert_matrix(Rational, Int16, 2)
+2×2 Array{Rational{Int16},2}:
+ 1//1  1//2
+ 1//2  1//3
+
+"""
+function hilbert_matrix(::Type{Q}, ::Type{S}, isqrt_nxn::I) where {I, Q, S}
+    return[Q{S}(1,((i)+j-1)) for i in 1:isqrt_nxn, j in 1:isqrt_nxn]
+end
+
 
