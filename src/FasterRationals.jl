97fd4ba5f82643efcf6ed918957e493dd6f231d7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 12:14:32 +0000

    Update FasterRationals.jl

--- src/FasterRationals.jl
+++ src/FasterRationals.jl
@@ -9,12 +9,23 @@ import Base: convert, promote_rule, string, show,
     (+), (-), (*), (//), div, rem, fld, mod, cld,
     (==), (<), (<=), isequal, isless
 
-#include(joinpath("types/struct","fast_rational.jl")
-#include(joinpath("types/mutablfeast_rational.jl")
-#include(joinpath("types/namedtuple/fast_rational.jl")
+import Base.Checked: add_with_overflow, sub_with_overflow, mul_with_overflow
 
-#include(joinpath("types/namedtuple", "compares.jl"))
-#include(joinpath("types/struct", "compares.jl"))
-#include(joinpath("types/mutable","compares.jl"))
+const SignedInt = Union{Int16, Int32, Int64, Int128, BigInt}
+
+
+#include("types/shared.jl")
+
+#include("types/namedtuple/fast_rational.jl")
+#include("types/struct/fast_rational.jl")
+#include("types/mutable//fast_rational.jl")
+
+#include("int_ops/namedtuple/fast_rational.jl")
+#include("int_ops/struct/fast_rational.jl")
+#include("int_ops/mutable/fast_rational.jl")
+
+#include(joinpath("types","namedtuple","compares.jl"))
+#include(joinpath("types","struct","compares.jl"))
+#include((joinpath("types","mutable","compares.jl"))
 
 end # FasterRationals
