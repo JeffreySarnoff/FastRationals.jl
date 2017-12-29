442969371c02424b5d30c3372cd082a7b4834647
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-29 04:42:59 +0000

    Update FasterRationals.jl
    #include("")

--- src/FasterRationals.jl
+++ src/FasterRationals.jl
@@ -1,1 +1,18 @@
+module FasterRationals
 
+export FastRational
+
+import Base: convert, promote_rule, string, show,
+    isfinite, isinteger,
+    signbit, flipsign, changesign,
+    (+), (-), (*), (//), div, rem, fld, mod, cld,
+    (==), (<), (<=), isequal, isless
+
+#include("")
+#include("")
+#include("")
+#include("")
+#include("")
+#include("")
+
+end # FasterRationals
