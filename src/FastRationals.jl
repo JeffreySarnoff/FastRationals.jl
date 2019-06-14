60f35d6058444b8ae01fdde0f9e6eb7caf4b5a88
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-14 18:03:38 -0400

    improve heavy throughput by 20%
    inlining `mulwider`

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -142,7 +142,7 @@ end
 end 
 
 
-mulwider(x::T, y::T) where {T<:Integer} = usewidemul(x,y) ? widemul(x,y) : x*y
+@inline mulwider(x::T, y::T) where {T<:Integer} = usewidemul(x,y) ? widemul(x,y) : x*y
 @inline usewidemul(x::T, y::T) where {T<:Integer} =
     (leading_zeros(abs(x)) + leading_zeros(abs(y))) < 8*sizeof(T)
     
