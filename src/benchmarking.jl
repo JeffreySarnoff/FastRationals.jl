ade15b0987db1ebdd6d55e2b52fea778db18143c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 16:29:32 -0400

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -16,15 +16,16 @@ function fastsum2third(::Type{T}, n) where {T}
 end
 
 function sum2third(::Type{T}, n) where {T}
-    onethird (one(eltype(T)) //  3*one(eltype(T)))
+    onethird = (one(eltype(T)) //  3*one(eltype(T)))
     a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
     while n>0
-      n = n - 1
-      t = t * a1
-      s = s + t
+     n = n - 1
+     t = t * a1
+     s = s + t
     end
     return s, t
 end
+
 
 sum2third(Rational{Int64},17)
 (1743392200//3486784401, 1//3486784401)
