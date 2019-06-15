1c59d896b0be9ad3b3ae81795949225edc4dc8f1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 15:51:16 -0400

    Create benchmarking.jl

---
+++ src/benchmarking.jl
@@ -0,0 +1,23 @@
+FastRationals.FastRational{T}(x::T) = FastRational{T,IsReduced}(x)
+
+Base.eltype(::Type{Rational{T}}) where {T} = T
+Base.eltype(::Type{FastRational{T,H}}) where {T,H} = T
+Base.eltype(x::Rational{T}) where {T} = T
+Base.eltype(x::FastRational{T,H}) where {T,H} = T
+
+
+function sum2third(::Type{T}, n) where {T}
+    onethird = T(one(eltype(T)), 3*one(eltype(T)))
+    a1 = a; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
+    while n>0
+      n = n - 1
+      t = t * a1
+      s = s + t
+    end
+    return s, t
+end
+
+
+sum2third(Rational{Int64},17)
+(1743392200//3486784401, 1//3486784401)
+
