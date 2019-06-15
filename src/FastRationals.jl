da9958894eb1fdb6ad72a0a9f9db85ddcc8819ce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 09:52:33 -0400

    simplify `mulwider`

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -149,10 +149,8 @@ end
     return num, den
 end 
 
-
-@inline mulwider(x::T, y::T) where {T<:Integer} = usewidemul(x,y) ? widemul(x,y) : x*y
-@inline usewidemul(x::T, y::T) where {T<:Integer} =
-    (leading_zeros(abs(x)) + leading_zeros(abs(y))) < 8*sizeof(T)
+# optimize for FastRational{Int32} using Int64 cross-multiplication for comparisons
+@inline mulwider(x::T, y::T) where {T<:Integer} = widemul(x,y)
     
 (==)(x::FastRational{T,IsReduced}, y::FastRational{T,IsReduced}) where {T} =
     x.num === y.num && x.den === y.den
