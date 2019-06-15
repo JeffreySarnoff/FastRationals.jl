dcec2d0feb6ddf9499b30205452a06aeb9ba7729
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 16:24:32 -0400

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -1,4 +1,9 @@
+
 FastRationals.FastRational{T}(x::T) = FastRational{T,IsReduced}(x)
+
+FastRational{Int64,H}(x::FastRational{Int64,IsReduced}, y::FastRational{Int64,MayReduce}) where {H<:Main.FastRationals.RationalState} =
+    x * inv(FastRational(canonical(y)))
+
 
 Base.eltype(::Type{Rational{T}}) where {T} = T
 Base.eltype(::Type{FastRational{T,H}}) where {T,H} = T
@@ -6,8 +11,8 @@ Base.eltype(x::Rational{T}) where {T} = T
 Base.eltype(x::FastRational{T,H}) where {T,H} = T
 
 
-function sum2third(::Type{T}, n) where {T}
-    onethird = T(one(eltype(T)), 3*one(eltype(T)))
+function fastsum2third(::Type{T}, n) where {T}
+    onethird = FastRational(one(eltype(T)) //  3*one(eltype(T)))
     a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
     while n>0
       n = n - 1
@@ -17,8 +22,23 @@ function sum2third(::Type{T}, n) where {T}
     return s, t
 end
 
+function sum2third(::Type{T}, n) where {T}
+    onethird (one(eltype(T)) //  3*one(eltype(T)))
+    a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
+    while n>0
+      n = n - 1
+      t = t * a1
+      s = s + t
+    end
+    return s, t
+end
 
 sum2third(Rational{Int64},17)
 (1743392200//3486784401, 1//3486784401)
 
 sum2third(FastRational{Int64},17)
+
+
+julia> sum2third(FastRational{Int64},17)
+(1743392200//3486784401, 1//3486784401)
+
