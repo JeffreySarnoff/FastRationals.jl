9ce758b49f66eda744a0121003ba53b185e10738
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 15:57:58 -0400

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -8,7 +8,7 @@ Base.eltype(x::FastRational{T,H}) where {T,H} = T
 
 function sum2third(::Type{T}, n) where {T}
     onethird = T(one(eltype(T)), 3*one(eltype(T)))
-    a1 = a; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
+    a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
     while n>0
       n = n - 1
       t = t * a1
@@ -21,3 +21,4 @@ end
 sum2third(Rational{Int64},17)
 (1743392200//3486784401, 1//3486784401)
 
+sum2third(FastRational{Int64},17)
