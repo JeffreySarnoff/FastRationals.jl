c7699f01f2d10afe8b0afc8bfbb98e61986989c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-15 16:33:14 -0400

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -4,7 +4,7 @@ Base.eltype(x::Rational{T}) where {T} = T
 Base.eltype(x::FastRational{T,H}) where {T,H} = T
 
 
-function fastsum2third(::Type{T}, n) where {T}
+function fast_sumstoward_half(::Type{T}, n) where {T}
     onethird = FastRational(one(eltype(T)) //  3*one(eltype(T)))
     a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
     while n>0
@@ -15,7 +15,7 @@ function fastsum2third(::Type{T}, n) where {T}
     return s, t
 end
 
-function sum2third(::Type{T}, n) where {T}
+function sums_toward_half(::Type{T}, n) where {T}
     onethird = (one(eltype(T)) //  3*one(eltype(T)))
     a1 = onethird; a2 = a1*a1; s = a1 + a2; t = a2*a1; s=s+t
     while n>0
@@ -27,10 +27,10 @@ function sum2third(::Type{T}, n) where {T}
 end
 
 
-sum2third(Rational{Int64},17)
+sums_toward_half(Rational{Int64},17)
 (1743392200//3486784401, 1//3486784401)
 
-sum2third(FastRational{Int64},17)
+sums_toward_half(FastRational{Int64},17)
 
 
 julia> sum2third(FastRational{Int64},17)
