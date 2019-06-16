0530303d6489a8591fc1fcf211db1632b878448b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 07:25:43 -0400

    Update prechecked.jl

--- src/prechecked.jl
+++ src/prechecked.jl
@@ -1,7 +1,3 @@
-const FastSigned   = Union{Int8, Int16, Int32, Int64}
-const FastUnsigned = Union{UInt8, UInt16, UInt32, UInt64}
-const FastInteger  = Union{FastSigned, FastUnsigned}
-
 safemul(x::T, y::T) where {T<:FastInteger} =
     usewidemul(x,y) ? widemul(x,y) : x*y
 
