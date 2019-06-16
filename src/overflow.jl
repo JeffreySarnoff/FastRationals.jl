fe47ebf6d0a10096e68d2183f3e762b4db067695
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 07:05:03 -0400

    Update overflow.jl

--- src/overflow.jl
+++ src/overflow.jl
@@ -1,46 +1,45 @@
-
-# core parts of add, sub
+# core parts of add, sub, mul, div
 
 @inline function addovf(x, y)
     ovf = false
-    numer, ovfl = mul_with_overflow(numerator(x), denominator(y)) # here, numer is a temp
+    numer, ovfl = mul_with_overflow(x.num, x.den) # here, numer is a temp
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), numerator(y)) # here, denom is a temp
+    denom, ovfl = mul_with_overflow(x.den, x.num) # here, denom is a temp
     ovf |= ovfl
     numer, ovfl = add_with_overflow(numer, denom) # numerator of sum
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), denominator(y)) # denominator of sum
+    denom, ovfl = mul_with_overflow(x.den, y.den) # denominator of sum
     ovf |= ovfl
     return numer, denom, ovf
 end
 
 @inline function subovf(x, y)
     ovf = false
-    numer, ovfl = mul_with_overflow(numerator(x), denominator(y)) # here, numer is a temp
+    numer, ovfl = mul_with_overflow(x.num, x.den) # here, numer is a temp
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), numerator(y)) # here, denom is a temp
+    denom, ovfl = mul_with_overflow(x.den, y.num) # here, denom is a temp
     ovf |= ovfl
     numer, ovfl = sub_with_overflow(numer, denom) # numerator of difference
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), denominator(y)) # denominator of difference
+    denom, ovfl = mul_with_overflow(x.den, y.den) # denominator of difference
     ovf |= ovfl
     return numer, denom, ovf
 end
 
 @inline function mulovf(x, y)
     ovf = false
-    numer, ovfl = mul_with_overflow(numerator(x), numerator(y))
+    numer, ovfl = mul_with_overflow(x.num, y.num)
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), denominator(y))
+    denom, ovfl = mul_with_overflow(x.den, y.den)
     ovf |= ovfl
     return numer, denom, ovf
 end
 
 @inline function divovf(x, y)
     ovf = false
-    numer, ovfl = mul_with_overflow(numerator(x), denominator(y))
+    numer, ovfl = mul_with_overflow(x.num, y.den)
     ovf |= ovfl
-    denom, ovfl = mul_with_overflow(denominator(x), numerator(y))
+    denom, ovfl = mul_with_overflow(x.den, y.num)
     ovf |= ovfl
     return numer, denom, ovf
 end
