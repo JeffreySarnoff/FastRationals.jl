7b6bd74236bd8b2a2000489b7d2864b0dfe33cbd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 01:41:15 -0400

    Update benchmarking.jl

--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -1,8 +1,4 @@
-slow17 = sums_toward_half(Rational{Int64},17)
-# (1743392200//3486784401, 1//3486784401)
 
-fast17 = sums_toward_half(FastRational{Int64, IsReduced},17)
-# (1743392200//3486784401, 1//3486784401)
 
 function sums_toward_half(::Type{T}, n) where {T}
     athird = onethird(T)
@@ -21,3 +17,10 @@ function onethird(::Type{Q}) where Q
     T = basistype(Q)
     return Q(one(T), one(T)+one(T)+one(T))
 end
+
+
+slow17 = sums_toward_half(Rational{Int64},17)
+# (1743392200//3486784401, 1//3486784401)
+
+fast17 = sums_toward_half(FastRational{Int64, IsReduced},17)
+# (1743392200//3486784401, 1//3486784401)
