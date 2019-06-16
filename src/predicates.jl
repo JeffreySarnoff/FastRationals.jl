cc2b8f9034a4db617f0a64616344118e30a229f7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 04:29:18 -0400

    Update predicates.jl

--- src/predicates.jl
+++ src/predicates.jl
@@ -11,8 +11,8 @@ mayreduce(x::FastRational{T,MayReduce}) where {T} = true
 # denominator is strictly positive
 # numerator   is negative, zero, or positve
 
-zero(::Type{FastRational{T,IsReduced}}) where {T,H} = FastRational{T,IsReduced}(zero(T), one(T))
-one(::Type{FastRational{T,IsReduced}})  where {T,H} = FastRational{T,IsReduced}(one(T),  one(T))
-
 iszero(x::FastRational{T,IsReduced}) where {T} = iszero(x.num)
 isone(x::FastRational{T,IsReduced})  where {T} = x.num === x.den
+
+isinteger(x::FastRational{T,IsReduced}) where {T} = x.den == 1
+isinteger(x::FastRational{T,MayReduce}) where {T} = canonical(x.num,x.den)[2] == 1
