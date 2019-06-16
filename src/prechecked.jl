5ac658f24eb378c5bf5f5f4a0044077009752efd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 07:00:20 -0400

    Update prechecked.jl

--- src/prechecked.jl
+++ src/prechecked.jl
@@ -12,5 +12,3 @@ end
 @inline function usewidemul(x::T, y::T) where {T<:FastUnsigned}
     iszero( (leading_zeros(x) + leading_zeros(t)) >> sizeof(T) )
 end
-
-safemul(x::T, y::T) where {T<:Signed} =
