3bd60d4ba782f2f5a1004e96cc5a2589dd41119a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-16 18:15:29 -0400

    Update prechecked.jl

--- src/prechecked.jl
+++ src/prechecked.jl
@@ -9,28 +9,29 @@
 =#
 
 """
-    propinquity(x::T, y::T)::Union{T, widen(T)}
+    propinquity(x::`T`, y::`T`)::Union{`T`, widen(`T`)}
 
-x * y, without possiblity of overflow, T is a very fast system integer
+**x * y, without possiblity of overflow**
+
+> `T` in {[U]Int8, [U]Int16, [U]Int32}
 
 
 Implict provision of the return type selects either the args' shared type or the nearest larger type.
 Using `Int16` args allows their product to be computed either as an `Int16` or as an `Int32`.
-
-
-
 A wider type is used when necessary to preclude overflow, which itself protects the computation from
 that sort of error.  When the originating type properly holds the product, the same type is used.
 """
 propinquity(x::T, y::T) where {T<:FastInteger} =
     usewidemul(x,y) ? widemul(x,y) : x*y
 
+
 """
     Rapid determination of the working Type
 
+```
 We are given two range-limited integers of the same
-performant system type, either signed or unsigned.
-We are asked : "Do you assert that this type would
+performant system type, either signed or unsigned.    
+We are asked : "Do you assert that this type would    
 hold their product precisly, with full accuracy?"
 
 `true` means
@@ -41,6 +42,7 @@ hold their product precisly, with full accuracy?"
    It is asserted that at least of (a) or (b) hold.
      (a) It is known that use of with the given type nay promulage inaccuracies.
      (b) It is not known whether the precision of the given type covers this product.
+```
 """ usewidemul
 
 @inline function usewidemul(x::T, y::T) where {T<:FastSigned}
