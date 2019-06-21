f20f099ef3f76e19bf320e2ad75c87a0f6913e47
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-20 23:50:07 -0400

    Update tests.jl
    revised to reflect the latest implementation

--- test/tests.jl
+++ test/tests.jl
@@ -1,25 +1,20 @@
 
 @testset "avoid overflow during add/subtract" begin
-    x = 2*10^11
+    x =  div(typemax(Int32),6)
     @test FastRational(1, 2x) + FastRational(1, 3x) == FastRational(5, 6x)
     @test FastRational(1, 2x) - FastRational(1, 3x) == FastRational(1, 6x)
 end
 
 @testset "constructors1" begin
-    @test FastRational(2,4).num == 1
-    @test FastRational(2,4).den == 2
-    @test FastRational((2,4)).num == 2
-    @test FastRational((2,4)).den == 4
+    @test FastRational(2,4).num == 2
+    @test FastRational(2,4).den == 4
+    @test FastRational((2,4)).num == 1
+    @test FastRational((2,4)).den == 2
 end
 
 @testset "convert from Rational" begin
     @test FastRational(1//2) == FastRational(1, 2)
-    @test FastRational(2, 4) == FastRational(1, 2)
-end
-
-@testset "conversion type" begin
-    @test typeof(convert(FastRational{Int64,FastRationals.MayReduce}, FastRational(1,2))) ==
-    FastRational{Int64,FastRationals.MayReduce}
+    @test FastRational((2, 4)) == FastRational(1, 2)
 end
 
 @testset "inverse matrix" begin
