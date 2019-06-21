d9f531e899c714709be5718163ad87b57b67e206
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-20 22:10:09 -0400

    Update FastRationals.jl

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -13,6 +13,12 @@ FastRational(x::Rational{T}) where {T<:Union{Int8, Int16}} =
 FastRational(x::Rational{T}) where {T<:Union{Int64, Int128, BigInt}} =
     FastRational(Int32(x.num), Int32(x.den))
 
+FastRational(x:Int32) = FastRational(x.num, one(Int32))
+FastRational(x::T) where {T<:Union{Int8, Int16}} =
+    FastRational(x%Int32, one(Int32))
+FastRational(x::T) where {T<:Union{Int64, Int128, BigInt}} =
+    FastRational(Int32(x), one(Int32))
+
 Rational(x::FastRational) = x.num//x.den
 Rational{Int32}(x::FastRational) = x.num//x.den
 Rational{T}(x::FastRational) where {T} = (T)(x.num)//(T)(x.den)
