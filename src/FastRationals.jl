75f24f0b8cf5dc99a2cac61b037fb3d136273e7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-20 22:07:53 -0400

    Create FastRationals.jl

---
+++ src/FastRationals.jl
@@ -0,0 +1,23 @@
+module FastRationals
+
+export FastRational
+
+struct FastRational <: Real
+    num::Int32
+    den::Int32
+end
+
+FastRational(x::Rational{Int32}) = FastRational(x.num, x.den)
+FastRational(x::Rational{T}) where {T<:Union{Int8, Int16}} =
+    FastRational(x.num%Int32, x.den%Int32)
+FastRational(x::Rational{T}) where {T<:Union{Int64, Int128, BigInt}} =
+    FastRational(Int32(x.num), Int32(x.den))
+
+Rational(x::FastRational) = x.num//x.den
+Rational{Int32}(x::FastRational) = x.num//x.den
+Rational{T}(x::FastRational) where {T} = (T)(x.num)//(T)(x.den)
+
+
+    
+
+end # FastRationals
