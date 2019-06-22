34553438b30dd39c4a6ece44764842a2be5888f3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 17:54:47 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,7 @@ makedocs(
         "Overview" => "index.md",
         "Appropriate Uses" => "appropriate.md",
         "The Stateless Way" => "thestatelessway.md",
+        "Finding the Range" => "findingtherange.md",
         "Two States, Two Types" => "twostates_twotypes",
         "Parameterized Modality" => "parameterized_modality",
         "References" => "references.md"
