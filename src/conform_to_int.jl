98742f03c8bac7078d3e5bc81f9c2414aae42971
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 13:00:30 -0400

    Update conform_to_int.jl

--- src/conform_to_int.jl
+++ src/conform_to_int.jl
@@ -1,3 +1,32 @@
+round(::Type{Integer}, x::T) where {T<:FastRational} = round(Integer, x.num//x.den)
+round(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = round(I, x.num//x.den)
+ceil(::Type{Integer}, x::T) where {T<:FastRational} = ceil(Integer, x.num//x.den)
+ceil(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = ceil(I, x.num//x.den)
+floor(::Type{Integer}, x::T) where {T<:FastRational} = floor(Integer, x.num//x.den)
+floor(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = floor(I, x.num//x.den)
+trunc(::Type{Integer}, x::T) where {T<:FastRational} = trunc(Integer, x.num//x.den)
+trunc(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = trunc(I, x.num//x.den)
+
+round(::Type{Integer}, x::T, ::RoundingMode{:ToZero}) where {T<:Rational} = trunc(Integer, x.num//x.den)
+round(::Type{I}, x::T, ::RoundingMode{:ToZero}) where {I<:Integer, T<:FastRational} = trunc(I, x.num//x.den)
+round(::Type{Integer}, x::T, ::RoundingMode{:FromZero}) where {T<:Rational} = -trunc(Integer, -x.num//x.den)
+round(::Type{I}, x::T, ::RoundingMode{:FromZero}) where {I<:Integer, T<:FastRational} = -trunc(I, -x.num//x.den)
+round(::Type{Integer}, x::T, ::RoundingMode{:RoundUp}) where {T<:Rational} = ceil(Integer, x.num//x.den)
+round(::Type{I}, x::T, ::RoundingMode{:RoundUp}) where {I<:Integer, T<:FastRational} = ceil(I, x.num//x.den)
+round(::Type{Integer}, x::T, ::RoundingMode{:RoundDown}) where {T<:Rational} = floor(Integer, x.num//x.den)
+round(::Type{I}, x::T, ::RoundingMode{:RoundDown}) where {I<:Integer, T<:FastRational} = floor(I, x.num//x.den)
+round(::Type{Integer}, x::T, ::RoundingMode{:RoundNearest}) where {T<:Rational} = round(Integer, x.num//x.den)
+round(::Type{I}, x::T, ::RoundingMode{:RoundNearest}) where {I<:Integer, T<:FastRational} = round(I, x.num//x.den)
+
+round(x::FastRational{T}, ::RoundingMode{:ToZero}) where {T<:Integer} = trunc(T, x)
+round(x::FastRational{T}, ::RoundingMode{:FromZero}) where {T<:Integer} = -trunc(T, -x)
+round(x::FastRational{T}, ::RoundingMode{:RoundUp}) where {T<:Integer} = ceil(T, x)
+round(x::FastRational{T}, ::RoundingMode{:RoundDown}) where {T<:Integer} = floor(T, x)
+round(x::FastRational{T}, ::RoundingMode{:RoundNearest}) where {T<:Integer} = round(T, x, RoundNearest)
+
+#=
+this is adapted directly from base -- there are "what error gets thrown" testing conflicts
+
 trunc(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = convert(T, div(x.num,x.den))
 floor(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = convert(T, fld(x.num,x.den))
 ceil(::Type{I},  x::FastRational{T}) where {I<:Integer, T<:FastInt} = convert(T, cld(x.num,x.den))
@@ -30,3 +59,4 @@ round(::Type{Integer}, x::FastRational{T}, ::RoundingMode{:Up}) where {T<:FastIn
 round(::Type{Integer}, x::FastRational{T}, ::RoundingMode{:Down}) where {T<:FastInt} = round(T, x, RoundDown)
 round(::Type{Integer}, x::FastRational{T}, ::RoundingMode{:Nearest}) where {T<:FastInt} = round(T, x, RoundNearest)
 round(::Type{Integer}, x::FastRational{T}) where {T<:FastInt} = round(T, x, RoundNearest)
+=#
