7a6d5dc3b0fb720669e5baec64a038c379a4a226
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 03:28:06 -0400

    Update promote_convert.jl

--- src/promote_convert.jl
+++ src/promote_convert.jl
@@ -6,9 +6,13 @@ promote_rule(::Type{FastQ32}, ::Type{Float64}) = FastQ32
 convert(::Type{FastQ32}, x::Float64) = FastQ32(Rational{Int32}(x))
 promote_rule(::Type{FastQ32}, ::Type{Float32}) = FastQ32
 convert(::Type{Float32}, x::FastQ32) = FastQ32(Rational{Int32}(x))
+promote_rule(::Type{FastQ32}, ::Type{Float16}) = FastQ32
+convert(::Type{Float16}, x::FastQ32) = FastQ32(Rational{Int32}(x))
 
 promote_rule(::Type{I}, ::Type{FastQ32}) where {I<:Integer} = FastQ32
 convert(::Type{FastQ32}, x::I) where {I<:Integer} = FastQ32(Int32(x), one(Int32))
+
+FastQ32(x::F) where {F<:AbstractFloat} = convert(F, x)
 
 # ------------------------- FastQ64
 
@@ -18,6 +22,10 @@ promote_rule(::Type{FastQ64}, ::Type{Float64}) = FastQ64
 convert(::Type{FastQ64}, x::Float64) = FastQ32(Rational{Int64}(x))
 promote_rule(::Type{FastQ64}, ::Type{Float32}) = FastQ64
 convert(::Type{Float32}, x::FastQ64) = FastQ32(Rational{Int64}(x))
+promote_rule(::Type{FastQ64}, ::Type{Float16}) = FastQ64
+convert(::Type{Float16}, x::FastQ64) = FastQ32(Rational{Int64}(x))
 
 promote_rule(::Type{I}, ::Type{FastQ64}) where {I<:Integer} = FastQ64
 convert(::Type{FastQ64}, x::I) where {I<:Integer} = FastQ64(Int64(x), one(Int32))
+
+FastQ64(x::F) where {F<:AbstractFloat} = convert(F, x)
