4f4b394a0d158d88da091935fbe5ad415629d0e5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 13:52:49 -0400

    Update conform_to_int.jl

--- src/conform_to_int.jl
+++ src/conform_to_int.jl
@@ -1,11 +1,30 @@
-round(::Type{Integer}, x::T) where {T<:FastRational} = round(Integer, x.num//x.den)
-round(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = round(I, x.num//x.den)
-ceil(::Type{Integer}, x::T) where {T<:FastRational} = ceil(Integer, x.num//x.den)
+#=
 ceil(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = ceil(I, x.num//x.den)
-floor(::Type{Integer}, x::T) where {T<:FastRational} = floor(Integer, x.num//x.den)
 floor(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = floor(I, x.num//x.den)
-trunc(::Type{Integer}, x::T) where {T<:FastRational} = trunc(Integer, x.num//x.den)
 trunc(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = trunc(I, x.num//x.den)
+round(::Type{I}, x::T) where {I<:Integer, T<:FastRational} = round(I, x.num//x.den)
+
+ceil(::Type{Integer}, x::T) where {T<:FastRational} = ceil(Integer, x.num//x.den)
+floor(::Type{Integer}, x::T) where {T<:FastRational} = floor(Integer, x.num//x.den)
+trunc(::Type{Integer}, x::T) where {T<:FastRational} = trunc(Integer, x.num//x.den)
+round(::Type{Integer}, x::T) where {T<:FastRational} = round(Integer, x.num//x.den)
+=#
+
+ceil(x::FastRational{T}) where {I<:Integer, T<:FastInt} = FastRational{T}(numerator(ceil(T, x.num//x.den)), one(T))
+floor(x::FastRational{T}) where {I<:Integer, T<:FastInt} = FastRational{T}(numerator(floor(T, x.num//x.den)), one(T))
+trunc(x::FastRational{T}) where {I<:Integer, T<:FastInt} = FastRational{T}(numerator(trunc(T, x.num//x.den)), one(T))
+round(x::FastRational{T}) where {I<:Integer, T<:FastInt} = FastRational{T}(numerator(round(T, x.num//x.den)), one(T))
+
+ceil(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = ceil(I, x.num//x.den)
+floor(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = floor(I, x.num//x.den)
+trunc(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = trunc(I, x.num//x.den)
+round(::Type{I}, x::FastRational{T}) where {I<:Integer, T<:FastInt} = round(I, x.num//x.den)
+
+ceil(::Type{Integer}, x::FastRational{T}) where {T<:FastInt} = ceil(Integer, x.num//x.den)
+floor(::Type{Integer}, x::FastRational{T}) where {T<:FastInt} = floor(Integer, x.num//x.den)
+trunc(::Type{Integer}, x::FastRational{T}) where {T<:FastInt} = trunc(Integer, x.num//x.den)
+round(::Type{Integer}, x::FastRational{T}) where {T<:FastInt} = round(Integer, x.num//x.den)
+
 
 round(::Type{Integer}, x::T, ::RoundingMode{:ToZero}) where {T<:Rational} = trunc(Integer, x.num//x.den)
 round(::Type{I}, x::T, ::RoundingMode{:ToZero}) where {I<:Integer, T<:FastRational} = trunc(I, x.num//x.den)
