3cf673a15a8ac602ce840b30ae71300450f30099
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 03:30:28 -0400

    Update promote_convert.jl

--- src/promote_convert.jl
+++ src/promote_convert.jl
@@ -1,5 +1,17 @@
 # ------------------------- FastQ32
 
+
+float(x::FastQ32) = x.num / x.den
+Base.Float64(x::FastQ32)  = float(x)
+Base.Float32(x::FastQ32)  = Float32(float(x))
+Base.Float16(x::FastQ32)  = Float16(float(x))
+Base.BigFloat(x::FastQ32) = BigFloat(x.num) / BigFloat(x.den)
+Base.BigInt(x::FastQ32)   = BigInt(Rational(x))
+
+FastQ32(x::F) where {F<:AbstractFloat} = convert(F, x)
+
+# ------------------------- FastQ32
+
 promote_rule(::Type{Rational{T}}, ::Type{FastQ32}) where {T} = FastQ32
 convert(::Type{FastQ32}, x::Rational{T}) where {T} = FastQ32(Int32(x.num), Int32(x.den))
 promote_rule(::Type{FastQ32}, ::Type{Float64}) = FastQ32
@@ -12,7 +24,16 @@ convert(::Type{Float16}, x::FastQ32) = FastQ32(Rational{Int32}(x))
 promote_rule(::Type{I}, ::Type{FastQ32}) where {I<:Integer} = FastQ32
 convert(::Type{FastQ32}, x::I) where {I<:Integer} = FastQ32(Int32(x), one(Int32))
 
-FastQ32(x::F) where {F<:AbstractFloat} = convert(F, x)
+# ------------------------- FastQ64
+
+float(x::FastQ64) = x.num / x.den
+Base.Float64(x::FastQ64)  = float(x)
+Base.Float32(x::FastQ64)  = Float32(float(x))
+Base.Float16(x::FastQ64)  = Float16(float(x))
+Base.BigFloat(x::FastQ64) = BigFloat(x.num) / BigFloat(x.den)
+Base.BigInt(x::FastQ64)   = BigInt(Rational(x))
+
+FastQ64(x::F) where {F<:AbstractFloat} = convert(F, x)
 
 # ------------------------- FastQ64
 
@@ -27,5 +48,3 @@ convert(::Type{Float16}, x::FastQ64) = FastQ32(Rational{Int64}(x))
 
 promote_rule(::Type{I}, ::Type{FastQ64}) where {I<:Integer} = FastQ64
 convert(::Type{FastQ64}, x::I) where {I<:Integer} = FastQ64(Int64(x), one(Int32))
-
-FastQ64(x::F) where {F<:AbstractFloat} = convert(F, x)
