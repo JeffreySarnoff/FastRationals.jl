313283fcdbfe4f892ab7b4059dc44d6edc4db57b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 04:21:13 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,17 @@
+using Documenter, DoubleFloats
 
+makedocs(
+    modules = [FastRationals],
+    sitename = "FastRationals.jl",
+    authors = "Jeffrey Sarnoff",
+    pages = Any[
+        "Overview" => "index.md",
+        "Appropriate Uses" => "appropriate.md",
+        "References" => "references.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/FastRationals.jl.git",
+    target = "build"
+)
