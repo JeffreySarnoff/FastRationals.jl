107bbd86352993bdc6f78d968b53d03b48a3655d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 03:49:06 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using FastRationals, LinearAlgebra
 using Test
 
-include("FastQ32.jl")
-include("FastQ64.jl")
+include("baseFastQ32.jl")
+include("baseFastQ64.jl")
+
+include("moreFastQ32.jl")
+include("moreFastQ64.jl")
 
 include("tests.jl")
