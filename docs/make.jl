ea9d82e7d019bc1a4806b76d5902a2e15eec1b0d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-23 03:01:38 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,16 +5,11 @@ makedocs(
     sitename = "FastRationals.jl",
     authors = "Jeffrey Sarnoff",
     pages = Any[
-        "Overview" => "index.md",
-        "Appropriate Uses" => "appropriate.md",
         "Finding the Range" => "findingtherange.md",        
         "The Stateless Way" => "thestatelessway.md",
         "What cannot overflow?" => "mayoverflow.md",
         "Rational Magnitude In Action" => "rationalmagnitude.md",
         "A metaphor that illuminates" => "metaphoricalflashlight.md",
-        "Two States, Two Types" => "twostates_twotypes",
-        "Parameterized Modality" => "parameterized_modality",
-        "References" => "references.md"
     ]
 )
 
