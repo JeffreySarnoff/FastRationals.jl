afd9c443586a80f110d9d22d6d9cd742a585e237
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 09:40:00 -0400

    Update FastRationals.jl

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -1,11 +1,11 @@
 module FastRationals
 
-export FastRational, FastQ32, FastQ64,
+export FastRational, FastQ32, FastQ64, FastQ128,
        basetype
 
 using Base.Checked: add_with_overflow, sub_with_overflow, mul_with_overflow
 
-import Base: BitSigned, hash, show, repr, string, tryparse,
+import Base: Signed, BitSigned, hash, show, repr, string, tryparse,
     zero, one, iszero, isone, isinteger,
     numerator, denominator, eltype, convert, promote_rule, decompose,
     isinteger, typemax, typemin, sign, signbit, copysign, flipsign, abs, float,
@@ -19,20 +19,24 @@ const FastInt = Union{Int8, Int16, Int32, Int64}
 struct FastRational{T} <: Real
     num::T
     den::T
+       
+    FastRational{T}(num::T, den::T) where {T<:Signed} = new{T}(num, den)
 end
+
+numerator(x::FastRational{T}) where {T<:Signed} = x.num
+denominator(x::FastRational{T}) where {T<:Signed} = x.den
 
 const FastQ32 = FastRational{Int32}
 const FastQ64 = FastRational{Int64}
+const FastQ128 = FastRational{Int128}
 
 
-@inline function FastRational(num::T, den::T) where {T<:FastInt}
+@inline function FastRational(num::T, den::T) where {T<:Signed}
     iszero(den) && throw(DivideError)
-    ((den | num) === typemin(T) && (den !== num)) && 
-        throw(DomainError("FastRationals use symmetric Ints, typemin(T) is not used."))   
     return FastRational{T}(num, den)
 end
 
-function canonical(num::T, den::T) where {T<:BitSigned}
+function canonical(num::T, den::T) where {T<:Signed}
     num, den = flipsign(num, den), abs(den)
     gcdval = Base.gcd(num, den)
     gcdval === one(T) && return num, den
@@ -41,19 +45,16 @@ function canonical(num::T, den::T) where {T<:BitSigned}
     return num, den
 end
 
-include("FastQ32.jl")
-include("FastQ64.jl")
+canonical(num::T, den::T) where {T<:Integer} = Base.divgcd(num,den)
 
-numerator(x::FastRational{T}) where {T<:FastInt} = x.num
-denominator(x::FastRational{T}) where {T<:FastInt} = x.den
+FastRational(x::FastRational{T}) where {T<:Signed} = x
+FastRational{T}(x::FastRational{T}) where {T<:Signed} = x
+FastRational{T}(x::FastRational{T1}) where {T,T<:T1<:Signed} = FastRational{T}(x.num, x.den)
+FastRational{T}(num::T1, den::T1) where {T,T<:T1<:Signed} = FastRational{T}(T(num), T(den))
+FastRational{T}(numden::Tuple{T1,T1}) where {T,T<:T1<:Signed} = FastRational{T}(T.(canonical(numden[1], numden[2])))
 
-FastQ64(x::FastQ32) = FastQ64(Rational{Int64}(x.num//x.den))
-FastQ32(x::FastQ64) = FastQ32(Rational{Int32}(x.num//x.den))
-
-promote_rule(::Type{FastQ64}, ::Type{FastQ32}) = FastQ64
-convert(::Type{FastQ64}, x::FastQ32) = FastQ64(x)
-
-include("promote_convert.jl")
+include("generic.jl")
 include("conform_to_int.jl")
+include("promote_convert.jl")
 
 end # FastRationals
