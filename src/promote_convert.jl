e265ebff1b60af406b43134a282c7ca119d4ccf0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 09:34:38 -0400

    Update promote_convert.jl

--- src/promote_convert.jl
+++ src/promote_convert.jl
@@ -1,42 +1,42 @@
-float(x::FastRational{T}) where {T<:FastInt} = x.num / x.den
+float(x::FastRational{T}) where {T<:Signed} = x.num / x.den
 
-Base.Float64(x::FastRational{T}) where {T<:FastInt}  = float(x)
-Base.Float32(x::FastRational{T}) where {T<:FastInt}  = Float32(float(x))I
-Base.Float16(x::FastRational{T}) where {T<:FastInt}  = Float16(float(x))
-Base.BigFloat(x::FastRational{T}) where {T<:FastInt} = BigFloat(x.num) / BigFloat(x.den)
-Base.BigInt(x::FastRational{T}) where {T<:FastInt}   = isinteger(x) ? BigInt((x.num//x.den).num) :
-                                                          throw(InexactError)
+Base.Float64(x::FastRational{T}) where {T<:Signed}  = float(x)
+Base.Float32(x::FastRational{T}) where {T<:Signed}  = Float32(float(x))I
+Base.Float16(x::FastRational{T}) where {T<:Signed}  = Float16(float(x))
+Base.BigFloat(x::FastRational{T}) where {T<:Signed} = BigFloat(x.num) / BigFloat(x.den)
+Base.BigInt(x::FastRational{T}) where {T<:Signed}   = isinteger(x) ? BigInt((x.num//x.den).num) :
+                                                                         throw(InexactError)
 
-function FastRational{T}(x::F) where {T<:FastInt, F<:AbstractFloat}
+function FastRational{T}(x::F) where {T<:Signed, F<:AbstractFloat}
     !isfinite(x) && throw(DomainError("finite values only"))
     return FastRational{T}(rationalize(x))
 end
 
-# ------------------------- FastQ32
 
-promote_rule(::Type{Rational{T}}, ::Type{FastQ32}) where {T} = FastQ32
-convert(::Type{FastQ32}, x::Rational{T}) where {T} = FastQ32(Int32(x.num), Int32(x.den))
-promote_rule(::Type{FastQ32}, ::Type{Float64}) = FastQ32
-convert(::Type{FastQ32}, x::Float64) = FastQ32(Rational{Int32}(x))
-promote_rule(::Type{FastQ32}, ::Type{Float32}) = FastQ32
-convert(::Type{Float32}, x::FastQ32) = FastQ32(Rational{Int32}(x))
-promote_rule(::Type{FastQ32}, ::Type{Float16}) = FastQ32
-convert(::Type{Float16}, x::FastQ32) = FastQ32(Rational{Int32}(x))
+FastQ128(x::FastQ64) = FastQ128(Rational{Int128}(x.num//x.den))
+FastQ128(x::FastQ32) = FastQ128(Rational{Int128}(x.num//x.den))
+FastQ64(x::FastQ32) = FastQ64(Rational{Int64}(x.num//x.den))
+FastQ32(x::FastQ64) = FastQ32(Rational{Int32}(x.num//x.den))
+FastQ64(x::FastQ128) = FastQ64(Rational{Int64}(x.num//x.den))
+FastQ32(x::FastQ128) = FastQ32(Rational{Int32}(x.num//x.den))
 
-promote_rule(::Type{I}, ::Type{FastQ32}) where {I<:Integer} = FastQ32
-convert(::Type{FastQ32}, x::I) where {I<:Integer} = FastQ32(Int32(x), one(Int32))
+promote_rule(::Type{FastQ128}, ::Type{FastQ64}) = FastQ128
+promote_rule(::Type{FastQ128}, ::Type{FastQ32}) = FastQ128
+promote_rule(::Type{FastQ64}, ::Type{FastQ32}) = FastQ64
+convert(::Type{FastQ128}, x::FastQ64) = FastQ128(x)
+convert(::Type{FastQ128}, x::FastQ32) = FastQ128(x)
+convert(::Type{FastQ64}, x::FastQ32) = FastQ64(x)
+convert(::Type{FastQ64}, x::FastQ128) = FastQ64(x)
+convert(::Type{FastQ32}, x::FastQ128) = FastQ32(x)
+convert(::Type{FastQ32}, x::FastQ64) = FastQ32(x)
 
-
-# ------------------------- FastQ64
-
-promote_rule(::Type{Rational{T}}, ::Type{FastQ64}) where {T} = FastQ64
-convert(::Type{FastQ64}, x::Rational{T}) where {T} = FastQ64(Int64(x.num), Int64(x.den))
-promote_rule(::Type{FastQ64}, ::Type{Float64}) = FastQ64
-convert(::Type{FastQ64}, x::Float64) = FastQ64(Rational{Int64}(x))
-promote_rule(::Type{FastQ64}, ::Type{Float32}) = FastQ64
-convert(::Type{Float32}, x::FastQ64) = FastQ64(Rational{Int64}(x))
-promote_rule(::Type{FastQ64}, ::Type{Float16}) = FastQ64
-convert(::Type{Float16}, x::FastQ64) = FastQ65(Rational{Int64}(x))
-
-promote_rule(::Type{I}, ::Type{FastQ64}) where {I<:Integer} = FastQ64
-convert(::Type{FastQ64}, x::I) where {I<:Integer} = FastQ64(Int64(x), one(Int32))
+for (Q,T) in ((:FastQ32, :Int32), (:FastQ64, :Int64), (:FastQ128, :Int128))
+  @eval begin
+    promote_rule(::Type{Rational{T}}, ::Type{$Q}) where {T} = $Q
+    convert(::Type{$Q}, x::Rational{T}) where {T} = $Q($T(x.num), $T(x.den))
+    promote_rule(::Type{$Q}, ::Type{F}) where {F<:AbstractFloat} = $Q
+    convert(::Type{$Q}, x::F) where {F<:AbstractFloat} = $Q(Rational{$T}(x))
+    promote_rule(::Type{I}, ::Type{$Q}) where {I<:Integer} = $Q
+    convert(::Type{$Q}, x::I) where {I<:Integer} = $Q($T(x), one($T))
+  end
+end
