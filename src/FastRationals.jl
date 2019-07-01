c1b4a83c0aad5143aeeffd5f3f189b2310b54f0a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-01 06:46:52 -0400

    Update FastRationals.jl

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -20,11 +20,11 @@ struct FastRational{T} <: Real
     num::T
     den::T
        
-    FastRational{T}(num::T, den::T) where {T<:Signed} = new{T}(num, den)
+    FastRational{T}(num::T, den::T) where {T<:Integer} = new{T}(num, den)
 end
 
-numerator(x::FastRational{T}) where {T<:Signed} = x.num
-denominator(x::FastRational{T}) where {T<:Signed} = x.den
+numerator(x::FastRational{T}) where {T<:Integer} = x.num
+denominator(x::FastRational{T}) where {T<:Integer} = x.den
 
 const FastQ32 = FastRational{Int32}
 const FastQ64 = FastRational{Int64}
@@ -32,7 +32,7 @@ const FastQ128 = FastRational{Int128}
 const FastQBig = FastRational{BigInt}
 
 
-@inline function FastRational(num::T, den::T) where {T<:Signed}
+@inline function FastRational(num::T, den::T) where {T<:Integer}
     iszero(den) && throw(DivideError)
     return FastRational{T}(num, den)
 end
