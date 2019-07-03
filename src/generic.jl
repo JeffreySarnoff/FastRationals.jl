1dd5c02d428701332fd4f56203090048c51799f3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-03 18:51:21 -0400

    Update generic.jl

--- src/generic.jl
+++ src/generic.jl
@@ -1,11 +1,11 @@
-basetype(::Type{FastRational{T}}) where {T<:Signed} = T
-basetype(x::FastRational{T}) where {T<:Signed} = T
+basetype(::Type{FastRational{T}}) where {T<:SUN} = T
+basetype(x::FastRational{T}) where {T<:SUN} = T
 
-typemax(::Type{FastRational{T}}) where {T<:Signed} = FastRational{T}(typemax(T), one(T))
-typemin(::Type{FastRational{T}}) where {T<:Signed} = FastRational{T}(typemin(T), one(T))
+typemax(::Type{FastRational{T}}) where {T<:SUN} = FastRational{T}(typemax(T), one(T))
+typemin(::Type{FastRational{T}}) where {T<:SUN} = FastRational{T}(typemin(T), one(T))
 
-FastRational{T}(x::Bool) where {T<:Signed} = x ? one(FastRational{T}) : zero(FastRational{T})
-FastRational{T}(x::Signed) where {T<:Signed} = FastRational{T}(T(x), one(T))
+FastRational{T}(x::Bool) where {T<:SUN} = x ? one(FastRational{T}) : zero(FastRational{T})
+FastRational{T}(x::SUN) where {T<:SUN} = FastRational{T}(T(x), one(T))
 
 FastQ32(x::Rational{T}) where {T<:Union{Int8, Int16, Int32}} =
     FastQ32(x.num%Int32, x.den%Int32)
@@ -15,30 +15,30 @@ FastQ64(x::Rational{T}) where {T<:Union{Int8, Int16, Int32, Int64}} =
     FastQ64(x.num%Int64, x.den%Int64)
 FastQ64(x::Rational{T}) where {T<:Int128} =
     FastQ64(Int64(x.num), Int64(x.den))
-FastQ128(x::Rational{T}) where {T<:Signed} =
+FastQ128(x::Rational{T}) where {T<:SUN} =
     FastQ128(x.num%Int128, x.den%Int128)
 
 
-string(x::FastRational{T}) where {T<:Signed} = string(Rational{T}(x))
-show(io::IO, x::FastRational{T}) where {T<:Signed} = show(io, Rational{T}(x))
+string(x::FastRational{T}) where {T<:SUN} = string(Rational{T}(x))
+show(io::IO, x::FastRational{T}) where {T<:SUN} = show(io, Rational{T}(x))
 
-zero(::Type{FastRational{T}}) where {T<:Signed} = FastRational{T}(zero(T), one(T))
-zero(x::FastRational{T}) where {T<:Signed} = zero(FastRational{T})
-one(::Type{FastRational{T}}) where {T<:Signed} = FastRational{T}(one(T), one(T))
-one(x::FastRational{T}) where {T<:Signed} = one(FastRational{T})
+zero(::Type{FastRational{T}}) where {T<:SUN} = FastRational{T}(zero(T), one(T))
+zero(x::FastRational{T}) where {T<:SUN} = zero(FastRational{T})
+one(::Type{FastRational{T}}) where {T<:SUN} = FastRational{T}(one(T), one(T))
+one(x::FastRational{T}) where {T<:SUN} = one(FastRational{T})
 
-iszero(x::FastRational{T}) where {T<:Signed} = x.num === zero(T)
-isone(x::FastRational{T}) where {T<:Signed} = x.num === x.den
-isinteger(x::FastRational{T}) where {T<:Signed} = x.den === one(T) || canonical(x.num, x.den)[2] == one(T)
+iszero(x::FastRational{T}) where {T<:SUN} = x.num === zero(T)
+isone(x::FastRational{T}) where {T<:SUN} = x.num === x.den
+isinteger(x::FastRational{T}) where {T<:SUN} = x.den === one(T) || canonical(x.num, x.den)[2] == one(T)
 
-signbit(x::FastRational{T}) where {T<:Signed} = xor(signbit(x.num), signbit(x.den))
-sign(x::FastRational{T}) where {T<:Signed} = signbit(x) ? FastRational{T}(-one(T), one(T)) : FastRational{T}(one(T), one(T))
-abs(x::FastRational{T}) where {T<:Signed} = signbit(x) ? -x : x
--(x::FastRational{T}) where {T<:Signed} = x.num !== typemin(T) ? FastRational{T}(-x.num, x.den) : FastRational{T}(x.num, -x.den)
-inv(x::FastRational{T}) where {T<:Signed} = signbit(x) ? FastRational{T}(-x.den, -x.num) : FastRational{T}(x.den, x.num)
+signbit(x::FastRational{T}) where {T<:SUN} = xor(signbit(x.num), signbit(x.den))
+sign(x::FastRational{T}) where {T<:SUN} = signbit(x) ? FastRational{T}(-one(T), one(T)) : FastRational{T}(one(T), one(T))
+abs(x::FastRational{T}) where {T<:SUN} = signbit(x) ? -x : x
+-(x::FastRational{T}) where {T<:SUN} = x.num !== typemin(T) ? FastRational{T}(-x.num, x.den) : FastRational{T}(x.num, -x.den)
+inv(x::FastRational{T}) where {T<:SUN} = signbit(x) ? FastRational{T}(-x.den, -x.num) : FastRational{T}(x.den, x.num)
 
-copysign(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = signbit(y) ? -abs(x) : abs(x)
-flipsign(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = signbit(y) ? -x : x
+copysign(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = signbit(y) ? -abs(x) : abs(x)
+flipsign(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = signbit(y) ? -x : x
 
 wider(x::Int8) = x%Int16
 wider(x::Int16) = x%Int32
@@ -50,15 +50,15 @@ wider(x::BigInt) = x
 ==(x::FastRational{BigInt}, y::FastRational{BigInt}) = x.num * y.den == x.den * y.num
 !=(x::FastRational{BigInt}, y::FastRational{BigInt}) = x.num * y.den != x.den * y.num
 
-==(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) === wider(x.den) * wider(y.num)
-!=(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) !== wider(x.den) * wider(y.num)
->=(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) >= wider(x.den) * wider(y.num)
-<=(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) <= wider(x.den) * wider(y.num)
->(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) > wider(x.den) * wider(y.num)
-<(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = wider(x.num) * wider(y.den) < wider(x.den) * wider(y.num)
+==(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) === wider(x.den) * wider(y.num)
+!=(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) !== wider(x.den) * wider(y.num)
+>=(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) >= wider(x.den) * wider(y.num)
+<=(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) <= wider(x.den) * wider(y.num)
+>(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) > wider(x.den) * wider(y.num)
+<(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = wider(x.num) * wider(y.den) < wider(x.den) * wider(y.num)
 
 
-function +(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+function +(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, den, ovf = addovf(x, y)
     !ovf && return FastRational{T}(num, den)
     numer, denom = addq(wider(x.num), wider(x.den), wider(y.num), wider(y.den))
@@ -66,7 +66,7 @@ function +(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
     return FastRational{T}(T(numer), T(denom))
 end
 
-function -(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+function -(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, den, ovf = subovf(x, y)
     !ovf && return FastRational{T}(num, den)
     numer, denom = subq(wider(x.num), wider(x.den), wider(y.num), wider(y.den))
@@ -74,7 +74,7 @@ function -(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
     return FastRational{T}(T(numer), T(denom))
 end
 
-function *(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+function *(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, den, ovf = mulovf(x, y)
     !ovf && return FastRational{T}(num, den)
     numer, denom = mulq(wider(x.num), wider(x.den), wider(y.num), wider(y.den))
@@ -82,7 +82,7 @@ function *(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
     return FastRational{T}(T(numer), T(denom))
 end
 
-function /(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+function /(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, den, ovf = divovf(x, y)
     !ovf && return FastRational{T}(num, den)
     numer, denom = divq(wider(x.num), wider(x.den), wider(y.num), wider(y.den))
@@ -91,7 +91,7 @@ function /(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
 end
 
 
-@inline function addovf(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+@inline function addovf(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num1, ovf  = mul_with_overflow(x.num, y.den)
     num2, ovfl = mul_with_overflow(x.den, y.num)
     ovf |= ovfl
@@ -102,7 +102,7 @@ end
     return num, den, ovf
 end
 
-@inline function subovf(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+@inline function subovf(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num1, ovf  = mul_with_overflow(x.num, y.den)
     num2, ovfl = mul_with_overflow(x.den, y.num)
     ovf |= ovfl
@@ -113,14 +113,14 @@ end
     return num, den, ovf
 end
 
-@inline function mulovf(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+@inline function mulovf(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, ovf  = mul_with_overflow(x.num, y.num)
     den, ovfl = mul_with_overflow(x.den, y.den)
     ovf |= ovfl
     return num, den, ovf
 end
 
-@inline function divovf(x::FastRational{T}, y::FastRational{T}) where {T<:Signed}
+@inline function divovf(x::FastRational{T}, y::FastRational{T}) where {T<:SUN}
     num, ovf  = mul_with_overflow(x.num, y.den)
     den, ovfl = mul_with_overflow(x.den, y.num)
     ovf |= ovfl
@@ -128,7 +128,7 @@ end
 end
 
 
-@inline function addq(xnum::T, xden::T, ynum::T, yden::T) where {T<:Signed}
+@inline function addq(xnum::T, xden::T, ynum::T, yden::T) where {T<:SUN}
     num1 = xnum * yden
     num2 = xden * ynum
     num = num1 + num2
@@ -136,7 +136,7 @@ end
     return num, den
 end
 
-@inline function subq(xnum::T, xden::T, ynum::T, yden::T) where {T<:Signed}
+@inline function subq(xnum::T, xden::T, ynum::T, yden::T) where {T<:SUN}
     num1 = xnum * yden
     num2 = xden * ynum
     num = num1 - num2
@@ -144,28 +144,28 @@ end
     return num, den
 end
 
-@inline function mulq(xnum::T, xden::T, ynum::T, yden::T) where {T<:Signed}
+@inline function mulq(xnum::T, xden::T, ynum::T, yden::T) where {T<:SUN}
     num = xnum * ynum
     den = xden * yden
     return num, den
 end
 
-@inline function divq(xnum::T, xden::T, ynum::T, yden::T) where {T<:Signed}
+@inline function divq(xnum::T, xden::T, ynum::T, yden::T) where {T<:SUN}
     num = xnum * yden
     den = xden * ynum
     return num, den
 end
 
-function ^(x::FastRational{T}, y::Signed) where {T<:Signed}
+function ^(x::FastRational{T}, y::SUN) where {T<:SUN}
     num, den = wider(x.num)^y, wider(x.den)^y
     num, den = canonical(num, den)
     return FastRational{T}(T(num), T(den))
 end
 
-//(x::FastRational{T}, y::Signed) where {T<:Signed} = x / FastRational{T}(y)
-//(x::Signed, y::FastRational{T}) where {T<:Signed} = FastRational{T}(x) / y
-//(x::FastRational{T}, y::FastRational{T}) where {T<:Signed} = x / y
-//(x::FastRational{T}, y::Rational) where {T<:Signed} = x / FastRational{T}(y)
-//(x::Rational, y::FastRational{T}) where {T<:Signed} = FastRational{T}(x) / y
+//(x::FastRational{T}, y::SUN) where {T<:SUN} = x / FastRational{T}(y)
+//(x::SUN, y::FastRational{T}) where {T<:SUN} = FastRational{T}(x) / y
+//(x::FastRational{T}, y::FastRational{T}) where {T<:SUN} = x / y
+//(x::FastRational{T}, y::Rational) where {T<:SUN} = x / FastRational{T}(y)
+//(x::Rational, y::FastRational{T}) where {T<:SUN} = FastRational{T}(x) / y
 
-decompose(x::FastRational{T}) where {T<:Signed} = x.num, zero(T), x.den
+decompose(x::FastRational{T}) where {T<:SUN} = x.num, zero(T), x.den
