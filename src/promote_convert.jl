3964ce54cdaa224856f06bbc36bff24ab56f705b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-03 18:37:29 -0400

    Update promote_convert.jl

--- src/promote_convert.jl
+++ src/promote_convert.jl
@@ -1,3 +1,5 @@
+FastRational(num::Integer, den::Integer) = FastRational(promote(num,den)...)
+
 Base.Rational{BQ}(x::FastRational{FQ}) where {BQ<:Integer, FQ<:Integer} = Rational(BQ(x.num), BQ(x.den))
 FastRational{FQ}(x::Rational{BQ}) where {FQ<:Integer, BQ<:Integer} = FastRational(FQ(x.num), FQ(x.den))
 FastRational{I1}(x::FastRational{I2}) where {I1<:Integer, I2<:Integer} = Rational(I1(x.num), I1(x.den))
