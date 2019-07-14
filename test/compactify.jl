40849b190a4d846d8ca6e0c48577bd93f94eae47
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-14 17:41:29 -0400

    Update compactify.jl

--- test/compactify.jl
+++ test/compactify.jl
@@ -1,4 +1,4 @@
-@testset "compactify $Q" for Q in (Rational{Int32}, Rational{Int64}, FastRational{Int32}, FastRational{Int64})
+@testset "compactify $Q" for Q in (FastQ16, FastQ32, FastQ64)
     mid = Q(100, 111)
     tol = Q(1, 11)
     res = Q(5, 6)
