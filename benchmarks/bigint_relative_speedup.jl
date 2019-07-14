05f8f5fd8fcfb54914f3a313ce30072344b18118
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-14 08:28:20 -0400

    Update bigint_relative_speedup.jl

--- benchmarks/bigint_relative_speedup.jl
+++ benchmarks/bigint_relative_speedup.jl
@@ -34,11 +34,16 @@ fq500sum = @belapsed sum($fq500);
 q500prod = @belapsed prod($q500);
 fq500prod = @belapsed prod($fq500);
 
-println(string("sum 200 Rational, reltime = $(round(q200sum/fq200sum))"))
-println(string("sum 500 Rational, reltime = $(round(q500sum/fq500sum))"))
-println("")
-println(string("prod 200 Rational, reltime = $(round(q200prod/fq200prod))"))
-println(string("prod 500 Rational, reltime = $(round(q500prod/fq500prod))"))
+relspeed_200sum =
+  round( (q200sum / fq200sum), digits=1);
+relspeed_200prod =
+  round( (q200prod / fq200prod), digits=1);
+
+relspeed_500sum =
+  round( (q500sum / fq500sum), digits=1);
+relspeed_500prod =
+  round( (q500prod / fq500prod), digits=1);
+
 
 q8x8 = reshape(q200[1:64], (8,8));
 fq8x8 = reshape(fq200[1:64], (8,8));
@@ -55,9 +60,29 @@ fq8x8tr = @belapsed tr($fq8x8);
 q15x15tr = @belapsed tr($q15x15);
 fq15x15tr = @belapsed tr($fq15x15);
 
-println(string("matmul 8x8 Rational, reltime = $(round(q8x8mul/fq8x8mul))"))
-println(string("matmul 15x15 Rational, reltime = $(round(q15x15mul/fq15x15mul))"))
-println("")
-println(string("mat_tr 8x8 Rational, reltime = $(round(q8x8tr/fq8x8tr))"))
-println(string("mat_tr 15x15 Rational, reltime = $(round(q15x15tr/fq15x15tr))"))
+relspeed_8x8mul =
+  round( (q8x8mul / fq8x8mul), digits=1);
+relspeed_8x8tr =
+  round( (q8x8tr / fq8x8tr), digits=1);
 
+relspeed_15x15mul =
+  round( (q15x15mul / fq15x15mul), digits=1);
+relspeed_15x15tr =
+  round( (q15x15tr / fq15x15tr), digits=1);
+
+
+relspeeds = string(
+"\n\n\trelative speeds",
+"\n\t (200)\t (500)\n\n",
+"sum:   \t $relspeed_200sum \t $relspeed_500sum \n",
+"prod:  \t $relspeed_200prod \t $relspeed_500prod \n");
+
+print(relspeeds);
+
+relspeeds = string(
+"\n\n\trelative speeds",
+"\n\t (8x8)\t (15x15)\n\n",
+"matmul:\t $relspeed_8x8mul \t $relspeed_15x15mul\n",
+"mat tr: \t $relspeed_8x8tr \t $relspeed_15x15tr \n");
+
+print(relspeeds);
