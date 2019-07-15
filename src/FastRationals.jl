6bc128abe03567757dbdfb8e5ae62c162e8a0c55
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-15 00:22:55 -0400

    Update FastRationals.jl

--- src/FastRationals.jl
+++ src/FastRationals.jl
@@ -25,7 +25,10 @@ struct FastRational{T} <: Real
         new{T}(numden[1], numden[2])
    
     FastRational(num::T, den::T) where {T<:SUN} =
-        new{T}(num, den)
+        iszero(den) && throw(DivideError)
+        num, den = flipsign(num, den), abs(den)
+        return new{T}(num, den)
+    end
      
     function FastRational{T}(num::T, den::T) where {T<:SUN}
         # iszero(den) && throw(DivideError)
