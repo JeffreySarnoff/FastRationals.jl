891fa0f34b5ede29e0c8ed7babcaf59aa5af8d72
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-15 22:29:37 -0400

    Update BBP_for_pi.jl

--- benchmarks/BBP_for_pi.jl
+++ benchmarks/BBP_for_pi.jl
@@ -31,39 +31,48 @@ function bpp(::Type{T}, n) where {T}
     return result
 end
 
+#=
+Computing Bailey–Borwein–Plouffe formula for Pi
+to ascertain the performance of FastRational{BigInt}
+relative to Rational{BigInt} by BPP iterations
+=#
 
 # err ~1e-54, 1_328 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   10);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 10);
-bpp10 = round(systemqtime/fastqtime, digits=1)
-
-# err ~1e-102, 4_671 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   25);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 25);
-bpp25 = round(systemqtime/fastqtime, digits=1)
-
-# err ~1e247, 26_431 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   50);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 50);
-bpp50 = round(systemqtime/fastqtime, digits=1)
+systemqtime = @belapsed bpp(Rational{BigInt},   15);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 15);
+bpp15 = round(fasttime/systemqqtime, digits=1)
 
 # err ~1e368, 57_914 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   100);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 100);
-bpp100 = round(systemqtime/fastqtime, digits=1)
+systemqtime = @belapsed bpp(Rational{BigInt},   125);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 125);
+bpp125 = round(fastqtime/systemqqtime, digits=1)
 
 # relspeeds meet at n=328
 
 # err ~1e368, 57_914 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   150);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 150);
-bpp150 = round(systemqtime/fastqtime, digits=1)
+systemqtime = @belapsed bpp(Rational{BigInt},   250);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 250);
+bpp250 = round(fastqtime/systemqtime, digits=1)
 
-# err ~1e368, 57_914 digits in num, den
-systemqtime = @belapsed bpp(Rational{BigInt},   200);
-fastqtime = @belapsed bpp(FastRational{BigInt}, 200);
-bpp200 = round(systemqtime/fastqtime, digits=1)
+systemqtime = @belapsed bpp(Rational{BigInt},   500);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 500);
+bpp500 = round(fastqtime/systemqtime, digits=1)
+
+systemqtime = @belapsed bpp(Rational{BigInt},   1000);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 1000);
+bpp1000 = round(fastqtime/systemqtime, digits=1)
+
+systemqtime = @belapsed bpp(Rational{BigInt},   2000);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 2000);
+bpp2000 = round(fastqtime/systemqtime, digits=1)
+
+systemqtime = @belapsed bpp(Rational{BigInt},   5000);
+fastqtime = @belapsed bpp(FastRational{BigInt}, 5000);
+bpp5000 = round(fastqtime/systemqtime, digits=1)
 
 
-relspeed = (bpp10=bpp10, bpp25=bpp25, bpp50=bpp50, bpp100=bpp100, bpp150=bpp150, bpp200=bpp200)
+
+relspeeds = (bpp15=bpp15, bpp125=bpp125, bpp250=bpp250, 
+             bpp500=bpp500, bpp1000=bpp1000, bpp2000=bpp2000,
+             bpp5000=bpp5000)
 
