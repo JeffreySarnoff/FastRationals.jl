c4c75ba680d8d2a0a54b77e15e9f68c631fd9182
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-21 19:26:01 -0400

    Update round_etc.jl

--- test/round_etc.jl
+++ test/round_etc.jl
@@ -11,3 +11,24 @@
     end
   end
 end
+
+@testset "enhanced rounding" begin
+  for I in (UInt8, UInt16, UInt32, UInt64, UInt128, 
+            Int8, Int16, Int32, Int64, Int128, BigInt)
+    @eval begin
+      fq = FastRational{$I}(33, 32)
+      fqf = float(fq)
+      @test round($I, fq, RoundUp) == round($I, fqf, RoundUp)
+      @test round($I, fq, RoundDown) == round($I, fqf, RoundDown)
+      @test round($I, fq, RoundToZero) == round($I, fqf, RoundToZero)
+      @test round($I, fq, RoundNearest) == round($I, fqf, RoundNearest)
+    end
+  end
+  fq = FastRational{Int64}(33, 32)
+  fqf = float(fq)
+  @test round(Integer, fq, RoundUp) == round(Integer, fqf, RoundUp)
+  @test round(Integer, fq, RoundDown) == round(Integer, fqf, RoundDown)
+  @test round(Integer, fq, RoundToZero) == round(Integer, fqf, RoundToZero)
+  @test round(Integer, fq, RoundNearest) == round(Integer, fqf, RoundNearest)
+end
+  
